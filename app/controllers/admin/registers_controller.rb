11ff12a2dd37bff1c0405d8b7b8f3c536ba2e2de
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-07-13 23:11:34 -0300

    refactor: Alterado service e controller de cadastro de funcionarios

--- app/controllers/admin/registers_controller.rb
+++ app/controllers/admin/registers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::RegistersController < ApplicationController
   before_action :require_admin
+
   def index
     @users = User.all
   end
@@ -13,104 +14,17 @@ class Admin::RegistersController < ApplicationController
   end
 
   def create
-    employee = Register::EmployeeRecords.new(
-      first_name: employee_params[:first_name],
-      last_name: employee_params[:last_name],
-      email_address: employee_params[:email_address],
-      active: employee_params[:active],
-      role: employee_params[:role],
-      password: employee_params[:password],
-      password_confirmation: employee_params[:password_confirmation],
-      hourly_rate: employee_params[:hourly_rate],
-      birth_date: employee_params[:birth_date],
-      gender: employee_params[:gender],
-      admission_date: employee_params[:admission_date],
-      address: employee_params[:address],
-      contract_type: employee_params[:contract_type],
-      salary: employee_params[:salary],
-      pis: employee_params[:pis],
-      ctps: employee_params[:ctps],
-      position: employee_params[:position],
-      cnpj: employee_params[:cnpj],
-      phone_number: employee_params[:phone_number],
-      pix_key: employee_params[:pix_key],
-      rg: employee_params[:rg],
-      cpf: employee_params[:cpf],
-      mother_name: employee_params[:mother_name],
-      mother_last_name: employee_params[:mother_last_name],
-      emergency_phone_number: employee_params[:emergency_phone_number],
-      nationality: employee_params[:nationality],
-      city_born: employee_params[:city_born],
-      uf_born: employee_params[:uf_born],
-      blood_group: employee_params[:blood_group],
-      allergies: employee_params[:allergies],
-      driver_license: employee_params[:driver_license],
-      driver_license_category: employee_params[:driver_license_category],
-      driver_license_number: employee_params[:driver_license_number],
-      cep: employee_params[:cep],
-      house_number: employee_params[:house_number],
-      neighborhood: employee_params[:neighborhood],
-      city: employee_params[:city],
-      uf_live: employee_params[:uf_live],
-      reference: employee_params[:reference],
-      user_id: nil
+    service = Register::EmployeeRecords.new(
+      user_attrs: user_params,
+      employee_attrs: employee_params
     )
 
-    if employee.call
+    if service.call
       redirect_to admin_registers_path, notice: "Funcionário cadastrado com sucesso!"
     else
+      @errors = service.errors
+      @user = User.new(user_params)
       render :new, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    employee = Register::EmployeeRecords.new(
-      first_name: employee_params[:first_name],
-      last_name: employee_params[:last_name],
-      email_address: employee_params[:email_address],
-      active: employee_params[:active],
-      role: employee_params[:role],
-      password: employee_params[:password],
-      password_confirmation: employee_params[:password_confirmation],
-      hourly_rate: employee_params[:hourly_rate],
-      birth_date: employee_params[:birth_date],
-      gender: employee_params[:gender],
-      admission_date: employee_params[:admission_date],
-      address: employee_params[:address],
-      contract_type: employee_params[:contract_type],
-      salary: employee_params[:salary],
-      pis: employee_params[:pis],
-      ctps: employee_params[:ctps],
-      position: employee_params[:position],
-      cnpj: employee_params[:cnpj],
-      phone_number: employee_params[:phone_number],
-      pix_key: employee_params[:pix_key],
-      rg: employee_params[:rg],
-      cpf: employee_params[:cpf],
-      mother_name: employee_params[:mother_name],
-      mother_last_name: employee_params[:mother_last_name],
-      emergency_phone_number: employee_params[:emergency_phone_number],
-      nationality: employee_params[:nationality],
-      city_born: employee_params[:city_born],
-      uf_born: employee_params[:uf_born],
-      blood_group: employee_params[:blood_group],
-      allergies: employee_params[:allergies],
-      driver_license: employee_params[:driver_license],
-      driver_license_category: employee_params[:driver_license_category],
-      driver_license_number: employee_params[:driver_license_number],
-      cep: employee_params[:cep],
-      house_number: employee_params[:house_number],
-      neighborhood: employee_params[:neighborhood],
-      city: employee_params[:city],
-      uf_live: employee_params[:uf_live],
-      reference: employee_params[:reference],
-      user_id: params[:id]
-    )
-
-    if employee.update
-      redirect_to admin_registers_path, notice: "Funcionário atualizado com sucesso!"
-    else
-      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -118,16 +32,35 @@ class Admin::RegistersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    service = Register::EmployeeRecords.new(
+      user_attrs: user_params,
+      employee_attrs: employee_params,
+      user_id: params[:id]
+    )
+
+    if service.update
+      redirect_to admin_registers_path, notice: "Funcionário atualizado com sucesso!"
+    else
+      @errors = service.errors
+      @user = User.find(params[:id])
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def user_params
+    params.require(:user).permit(
+      :first_name, :last_name, :email_address, :active, :role,
+      :password, :password_confirmation
+    ).to_h.reject { |_, v| v.blank? }
+  end
+
   def employee_params
-    params.permit(
-      :first_name, :last_name, :email_address, :active, :role, :password, :password_confirmation,
-      :hourly_rate, :user_id, :birth_date, :gender, :admission_date, :contract_type, :address, :salary,
-      :pis, :ctps, :position, :cnpj, :phone_number, :pix_key, :rg, :cpf, :mother_name, :mother_last_name,
-      :emergency_phone_number, :nationality, :city_born, :uf_born, :blood_group, :allergies,
-      :driver_license, :driver_license_category, :driver_license_number, :cep, :house_number,
-      :neighborhood, :city, :uf_live, :reference
-    )
+    params.require(:user).permit(
+      :hourly_rate, :birth_date, :gender, :admission_date,
+      :contract_type, :salary, :position, :phone_number, :rg, :cpf
+    ).to_h.reject { |_, v| v.blank? }
   end
 end
--- app/services/register/employee_records.rb
+++ app/services/register/employee_records.rb
@@ -1,54 +1,12 @@
 module Register
   class EmployeeRecords
-    def initialize(first_name:, last_name:, email_address:, active:, role:, password:,
-                   password_confirmation:, hourly_rate:, birth_date:, gender:, admission_date:,
-                   address:, contract_type:, salary:, pis:, ctps:, position:, cnpj:, phone_number:,
-                   pix_key:, rg:, cpf:, mother_name:, mother_last_name:, emergency_phone_number:,
-                   nationality:, city_born:, uf_born:, blood_group:, allergies:, driver_license:,
-                   driver_license_category:, driver_license_number:, cep:, house_number:,
-                   neighborhood:,
-                   city:, uf_live:, reference:, user_id:)
+    attr_reader :errors
 
-      @first_name = first_name
-      @last_name = last_name
-      @email_address = email_address
-      @active = active
-      @role = role
-      @password = password
-      @password_confirmation = password_confirmation
-      @birth_date = birth_date
-      @hourly_rate = hourly_rate
-      @gender = gender
-      @admission_date = admission_date
-      @address = address
-      @contract_type = contract_type
-      @salary = salary
-      @pis = pis
-      @ctps = ctps
-      @position = position
-      @cnpj = cnpj
-      @phone_number = phone_number
-      @pix_key = pix_key
-      @rg = rg
-      @cpf = cpf
-      @mother_name = mother_name
-      @mother_last_name = mother_last_name
-      @emergency_phone_number = emergency_phone_number
-      @nationality = nationality
-      @city_born = city_born
-      @uf_born = uf_born
-      @blood_group = blood_group
-      @allergies = allergies
-      @driver_license = driver_license
-      @driver_license_category = driver_license_category
-      @driver_license_number = driver_license_number
-      @cep = cep
-      @house_number = house_number
-      @neighborhood = neighborhood
-      @city = city
-      @uf_live = uf_live
-      @reference = reference
+    def initialize(user_attrs:, employee_attrs:, user_id: nil)
+      @user_attrs = user_attrs
+      @employee_attrs = employee_attrs
       @user_id = user_id
+      @errors = []
     end
 
     def call
@@ -61,106 +19,32 @@ module Register
 
     private
 
-    attr_reader :first_name, :last_name, :email_address, :active, :role, :password,
-                :password_confirmation, :hourly_rate, :contract_type, :salary, :pis, :ctps,
-                :position, :cnpj, :gender, :birth_date, :admission_date, :address, :phone_number,
-                :pix_key, :rg, :cpf, :mother_name, :mother_last_name, :emergency_phone_number,
-                :nationality, :city_born, :uf_born, :blood_group, :allergies, :driver_license,
-                :driver_license_category, :driver_license_number, :cep, :house_number,
-                :neighborhood, :city, :uf_live, :reference, :user_id
+    attr_reader :user_attrs, :employee_attrs, :user_id
 
     def create_new_employee
-      user = User.create!(
-        first_name: first_name,
-        last_name: last_name,
-        email_address: email_address,
-        active: active,
-        role: role,
-        password: password,
-        password_confirmation: password_confirmation
-      )
-      employee = Employee.create!(
-        hourly_rate: hourly_rate,
-        birth_date: birth_date,
-        gender: gender,
-        admission_date: admission_date,
-        address: address,
-        contract_type: contract_type,
-        salary: salary,
-        pis: pis,
-        ctps: ctps,
-        position: position,
-        cnpj: cnpj,
-        phone_number: phone_number,
-        pix_key: pix_key,
-        rg: rg,
-        cpf: cpf,
-        mother_name: mother_name,
-        mother_last_name: mother_last_name,
-        emergency_phone_number: emergency_phone_number,
-        nationality: nationality,
-        city_born: city_born,
-        uf_born: uf_born,
-        blood_group: blood_group,
-        allergies: allergies,
-        driver_license: driver_license,
-        driver_license_category: driver_license_category,
-        driver_license_number: driver_license_number,
-        cep: cep,
-        house_number: house_number,
-        neighborhood: neighborhood,
-        city: city,
-        uf_live: uf_live,
-        reference: reference,
-        user: user
-      )
+      ActiveRecord::Base.transaction do
+        user = User.create!(user_attrs)
+        user.build_employee(employee_attrs).save!
+      end
+      true
+    rescue ActiveRecord::RecordInvalid => e
+      @errors = e.record.errors.full_messages
+      false
     end
 
     def update_employee
-      user = User.find(user_id)
-      user.update!(
-        first_name: first_name,
-        last_name: last_name,
-        email_address: email_address,
-        active: active,
-        role: role
-      )
-
-      employee = user.employee
-      employee.update!(
-        hourly_rate: hourly_rate,
-        birth_date: birth_date,
-        gender: gender,
-        admission_date: admission_date,
-        address: address,
-        contract_type: contract_type,
-        salary: salary,
-        pis: pis,
-        ctps: ctps,
-        position: position,
-        cnpj: cnpj,
-        phone_number: phone_number,
-        pix_key: pix_key,
-        rg: rg,
-        cpf: cpf,
-        mother_name: mother_name,
-        mother_last_name: mother_last_name,
-        emergency_phone_number: emergency_phone_number,
-        nationality: nationality,
-        city_born: city_born,
-        uf_born: uf_born,
-        blood_group: blood_group,
-        allergies: allergies,
-        driver_license: driver_license,
-        driver_license_category: driver_license_category,
-        driver_license_number: driver_license_number,
-        cep: cep,
-        house_number: house_number,
-        neighborhood: neighborhood,
-        city: city,
-        uf_live: uf_live,
-        reference: reference
-      )
+      ActiveRecord::Base.transaction do
+        user = User.find(user_id)
+        user.update!(user_attrs)
+        user.employee.update!(employee_attrs)
+      end
+      true
+    rescue ActiveRecord::RecordNotFound
+      @errors = ["Funcionário não encontrado"]
+      false
+    rescue ActiveRecord::RecordInvalid => e
+      @errors = e.record.errors.full_messages
+      false
     end
   end
 end
