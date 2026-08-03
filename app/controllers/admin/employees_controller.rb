7cfa637008654a3a20a2c7e79fc6431e19d504df
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-08-03 01:31:23 -0300

    perf: implementado formObject para cadastro de funcionarios

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -10,57 +10,42 @@ class Admin::EmployeesController < ApplicationController
   end
 
   def new
-    @user = User.new
+    @form = EmployeeRegistrationForm.new
   end
 
   def create
-    service = EmployeesRegistration::CreateEmployee.new(
-      user_attrs: user_params,
-      employee_attrs: employee_params
-    )
+    @form = EmployeeRegistrationForm.new(registration_params)
 
-    if service.call
+    if @form.save
       redirect_to admin_employees_path, notice: "Funcionário cadastrado com sucesso!"
     else
-      @errors = service.errors
-      @user = User.new(user_params)
+      puts "ERROS DE VALIDAÇÃO: #{@form.errors.full_messages}"
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    @form = EmployeeRegistrationForm.from_user(user)
   end
 
   def update
-    service = EmployeesRegistration::CreateEmployee.new(
-      user_attrs: user_params,
-      employee_attrs: employee_params,
-      user_id: params[:id]
-    )
+    @form = EmployeeRegistrationForm.new(registration_params.merge(user_id: params[:id]))
 
-    if service.update
+    if @form.save
       redirect_to admin_employees_path, notice: "Funcionário atualizado com sucesso!"
     else
-      @errors = service.errors
-      @user = User.find(params[:id])
       render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
-  def user_params
+  def registration_params
     params.require(:user).permit(
-      :first_name, :last_name, :email_address, :active, :role,
-      :password, :password_confirmation
-    ).to_h.reject { |_, v| v.blank? }
-  end
-
-  def employee_params
-    params.require(:user).permit(
-      :hourly_rate, :birth_date, :gender, :admission_date,
-      :contract_type, :salary, :position, :phone_number, :rg, :cpf
-    ).to_h.reject { |_, v| v.blank? }
+      :first_name, :last_name, :email_address, :active, :role, :password, :password_confirmation,
+      :hourly_rate, :birth_date, :gender, :admission_date, :contract_type,
+      :salary, :position, :phone_number, :rg, :cpf
+    )
   end
 end
